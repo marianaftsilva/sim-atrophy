4c3133bf07633b9e99a8fb6be5d662dd5bb2b653
Mariana da Silva <63010552+marianaftsilva@users.noreply.github.com>
2025-02-07 13:50:08 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -18,64 +18,71 @@ from model.network_3d import SpatialTransform
 from model.losses_3d import gradient
 from utils.dataloader import DataSetEval
 
+
 def main():
-    
     # define args
     parser = OptionParser()
-    parser.add_option("--atr_path", dest="atr_folder", default='/atr_folder/')
-    parser.add_option("--img_path", dest="img_folder", default='/img_folder/')
-    parser.add_option("--parc_path", dest="parc_folder", default='/parc_folder/')
+    parser.add_option("--atr_path", dest="atr_folder", default='/data/ADNI_mariana/Train/')
+    parser.add_option("--seg_path", dest="seg_folder", default='/data/ADNI_mariana/Seg_down/')
+    parser.add_option("--parc_path", dest="parc_folder", default='/data/ADNI_mariana/Parcellations_down/')
+    parser.add_option("--img_path", dest="img_folder", default='/data/ADNI_mariana/T1_down/')
     parser.add_option("--atr_is_list", dest="atr_list", default=False)
     parser.add_option("--batch_n", dest="batch_size", default=1)
     parser.add_option("--gpu", dest='gpu', default=0)
-    parser.add_option("--load_model", dest='load_model', default='trained_model_3d.pt')
-    
+    parser.add_option("--load_model", dest='load_model', default='model_bio199.pt')
+
     (opts, args) = parser.parse_args()
-    
+
     device = torch.device('cuda:' + str(opts.gpu) if torch.cuda.is_available() else 'cpu')
-    
+
     # load data
-    ds_test = DataSetEval(atr_folder = opts.atr_folder, img_folder = opts.img_folder, parc_folder = opts.parc_folder, atr_list = opts.atr_list)
-    test_loader = torch.utils.data.DataLoader(ds_test, batch_size = opts.batch_size, shuffle = False) 
-    
-    model = torch.load(opts.load_model, map_location = device)
-    
+    ds_test = DataSetEval(atr_folder=opts.atr_folder, img_folder=opts.img_folder, parc_folder=opts.parc_folder, atr_list = True)
+    test_loader = torch.utils.data.DataLoader(ds_test, batch_size=opts.batch_size, shuffle=False)
+
+    model = torch.load(opts.load_model, map_location=device)
+
     model.eval()
     for k, (input_image, input_atrophy, affine, sub_id, original_size) in enumerate(test_loader, 0):
-
-        deformation = model(input_atrophy.to(device))
+        if input_atrophy.max() > 2:
+          break
+        deformation = model(atr1.to(device))
 
         # ATTENTION!!!
         # this is needed to fix a mismatch between the order of the axis defined in the biomechanical model
         # and the axis on the image. TO BE FIXED IN A FUTURE RELEASE       
         new_flow = torch.ones_like(deformation)
-        new_flow[:,0,:,:,:] = deformation[:,2,:,:,:]
-        new_flow[:,1,:,:,:] = deformation[:,0,:,:,:]
-        new_flow[:,2,:,:,:] = deformation[:,1,:,:,:]
-        
-        spatial_transform = SpatialTransform([input_image.shape[2],input_image.shape[3],input_image.shape[4]]).to(device)
-        
-        deformed_image = spatial_transform(input_image.to(device),new_flow)
-        
+        new_flow[:, 0, :, :, :] = deformation[:, 2, :, :, :]
+        new_flow[:, 1, :, :, :] = deformation[:, 0, :, :, :]
+        new_flow[:, 2, :, :, :] = deformation[:, 1, :, :, :]
+
+        spatial_transform = SpatialTransform([input_image.shape[2], input_image.shape[3], input_image.shape[4]]).to(
+            device)
+
+        deformed_image_2 = spatial_transform(deformed_image_1.to(device), new_flow)
+
         # save deformed image and flow with original image size
-        final_image = np.zeros((182,218,182))
-        final_image[2:178,4:212,2:178] = deformed_image.detach().cpu().numpy().squeeze(0).squeeze(0)
-        
-        flow_final = np.zeros((182,218,182,3))
-        flow_final[2:178,4:212,2:178,:] = new_flow.permute(0,2,3,4,1).detach().cpu().numpy().squeeze(0)
-        
+        final_image = np.zeros((182, 218, 182))
+        final_image[2:178, 4:212, 2:178] = deformed_image.detach().cpu().numpy().squeeze(0).squeeze(0)
+
+        flow_final = np.zeros((182, 218, 182, 3))
+        flow_final[2:178, 4:212, 2:178, :] = new_flow.permute(0, 2, 3, 4, 1).detach().cpu().numpy().squeeze(0)
+
         # get det(F), which corresponds to computed atrophy
-        F = gradient(deformation, device) 
-        F[:,:,:,:,0,0] = F[:,:,:,:,0,0] + 1
-        F[:,:,:,:,1,1] = F[:,:,:,:,1,1] + 1
-        F[:,:,:,:,2,2] = F[:,:,:,:,2,2] + 1
+        F = gradient(deformation, device)
+        F[:, :, :, :, 0, 0] = F[:, :, :, :, 0, 0] + 1
+        F[:, :, :, :, 1, 1] = F[:, :, :, :, 1, 1] + 1
+        F[:, :, :, :, 2, 2] = F[:, :, :, :, 2, 2] + 1
         det = torch.det(F).detach().cpu().numpy().squeeze(0)
-        
-        nib.save(nib.Nifti1Image(final_image, affine=affine.squeeze(0)), 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_deformed_img.nii.gz')  
-        nib.save(nib.Nifti1Image(flow_final, affine=affine.squeeze(0)), 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_deformation.nii.gz')
-        nib.save(nib.Nifti1Image(det, affine=affine.squeeze(0)), 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_estimated_atrophy.nii.gz')
-        
+
+        nib.save(nib.Nifti1Image(final_image, affine=affine.squeeze(0)),
+                 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_deformed_img.nii.gz')
+        nib.save(nib.Nifti1Image(flow_final, affine=affine.squeeze(0)),
+                 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_deformation.nii.gz')
+        nib.save(nib.Nifti1Image(det, affine=affine.squeeze(0)),
+                 'results/' + sub_id[0].split('.')[0].split('_to')[0] + '_estimated_atrophy.nii.gz')
+
     return
+
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -15,42 +15,43 @@ from utils.dataloader import DataSetTrain
 from model.network_3d import unet_core
 from model.losses_3d import boundary_constraint, W_neo_brain, center_constraint
 
+
 def main():
-    
     # define args
     parser = OptionParser()
     parser.add_option("--atr_path", dest="atr_folder", default='/data/ADNI_mariana/Train/')
     parser.add_option("--seg_path", dest="seg_folder", default='/data/ADNI_mariana/Seg_down/')
     parser.add_option("--parc_path", dest="parc_folder", default='/data/ADNI_mariana/Parcellations_down/')
+    parser.add_option("--img_path", dest="img_folder", default='/data/ADNI_mariana/T1_down/')
     parser.add_option("--atr_is_list", dest="atr_list", default=True)
     parser.add_option("--batch_n", dest="batch_size", default=32)
     parser.add_option("--gpu", dest='gpu', default=0)
     parser.add_option("--nf_enc", dest='nf_enc', default=[16, 32, 32, 32])
     parser.add_option("--nf_dec", dest='nf_dec', default=[32, 32, 32, 32, 8, 8])
-    parser.add_option("--epochs", dest='epochs', default= 200)
+    parser.add_option("--epochs", dest='epochs', default=200)
     parser.add_option("--n_save_iter", dest='n_save_iter', default=1)
     parser.add_option("--lr", dest='lr', default=1e-5)
     parser.add_option("--w_losses", dest='weight', default=100)
     parser.add_option("--load_model", dest='load_model', default=None)
-    
+
     (opts, args) = parser.parse_args()
-    
+
     device = torch.device('cuda:' + str(opts.gpu) if torch.cuda.is_available() else 'cpu')
     print(device)
-    
+
     # load data
-    ds_train = DataSetTrain(atr_folder = opts.atr_folder, seg_folder = opts.seg_folder, parc_folder = opts.parc_folder, atr_list = opts.atr_list)
-    train_loader = torch.utils.data.DataLoader(ds_train, batch_size = opts.batch_size, shuffle = True) 
-    
+    ds_train = DataSetTrain(atr_folder=opts.atr_folder, seg_folder=opts.seg_folder, parc_folder=opts.parc_folder,
+                            atr_list=opts.atr_list)
+    train_loader = torch.utils.data.DataLoader(ds_train, batch_size=opts.batch_size, shuffle=True)
+
     # load model 
     if isinstance(opts.load_model, type(None)):
-        model = unet_core(3, opts.nf_enc, opts.nf_dec, n_in = 1).to(device)
+        model = unet_core(3, opts.nf_enc, opts.nf_dec, n_in=1).to(device)
     else:
-        model = torch.load(opts.load_model, map_location = device)
+        model = torch.load(opts.load_model, map_location=device)
 
-    optimizer = Adam(model.parameters(), lr= opts.lr)
-    
-    
+    optimizer = Adam(model.parameters(), lr=opts.lr)
+
     for i in range(0, opts.epochs):
         model.train()
         loss_epoch = 0
@@ -58,46 +59,46 @@ def main():
         loss2_epoch = 0
         loss3_epoch = 0
         for k, (input_seg, input_atrophy, center, sub_id) in enumerate(train_loader, 0):
-            #print(k)
+            # print(k)
             input_atrophy = input_atrophy.to(device)
             input_seg = input_seg.to(device)
             center = center.to(device)
-            
+
             # create array of parameter miu 
             # miu = 1 for white and grey matter
             miu = torch.ones_like(input_seg)
             # miu = 0.01 for CSF
-            miu[input_seg==1] = 0.01
+            miu[input_seg == 1] = 0.01
             # miu = 0 for background
-            miu[input_seg==0] = 0
+            miu[input_seg == 0] = 0
             miu = miu.to(device)
-            
+
             deformation = model(input_atrophy)
-            
+
             # calculate losses
-            loss1 = W_neo_brain(deformation, input_atrophy, miu, device) / torch.numel(input_seg) 
-            loss2 = (boundary_constraint(deformation,input_seg) / torch.numel(input_seg)) * opts.weight/10
+            loss1 = W_neo_brain(deformation, input_atrophy, miu, device) / torch.numel(input_seg)
+            loss2 = (boundary_constraint(deformation, input_seg) / torch.numel(input_seg)) * opts.weight / 10
             loss3 = center_constraint(deformation, center)
-   
-            loss = loss1 + loss2 
-            
+
+            loss = loss1 + loss2
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             loss_epoch += loss
 
             loss1_epoch += loss1
             loss2_epoch += loss2
             loss3_epoch += loss3
-            
+
         if i % opts.n_save_iter == 0:
-    
-            print(i,loss_epoch.item()/k, loss1_epoch.item()/k, loss2_epoch.item()/k, loss3_epoch.item()/k )
-    
+            print(i, loss_epoch.item() / k, loss1_epoch.item() / k, loss2_epoch.item() / k, loss3_epoch.item() / k)
+
             torch.save(model, 'model_bio' + str(i) + '.pt')
-            
+
     return
+
 
 if __name__ == "__main__":
     main()
